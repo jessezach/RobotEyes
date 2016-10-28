49ac71552e5002b2b127ed1a32291d5c6985ffed
Jesse <jesse@practo.com>
2016-10-29 00:45:07 +0530

    initial commit

---
+++ RobotEyes.py
@@ -0,0 +1,127 @@
+from PIL import Image
+import subprocess
+import os, shutil
+from robot.libraries.BuiltIn import BuiltIn
+
+
+class RobotEyes(object):
+
+    def __init__(self, env, mode):
+        self.env = env
+        self.mode = mode
+        self.count = 1
+        self.root_path = os.path.dirname(os.path.abspath(__file__))
+
+    def open_eyes(self):
+        try:
+            s2l = BuiltIn().get_library_instance('Selenium2Library')
+            self.driver = s2l._current_browser()
+        except:
+            try:
+                appiumlib = BuiltIn().get_library_instance('AppiumLibrary')
+                self.driver = appiumlib._current_application()
+            except:
+                raise ValueError('Browser/App is not open')
+
+        self.test_name = BuiltIn().replace_variables('${TEST NAME}')
+
+        if self.mode == 'test':
+            if os.path.exists(self.root_path + '/actual/' + self.test_name):
+                shutil.rmtree(self.root_path + '/actual/' + self.test_name)
+
+            if os.path.exists(self.root_path + '/diff/' + self.test_name):
+                shutil.rmtree(self.root_path + '/diff/' + self.test_name)
+
+        elif self.mode == 'baseline':
+            if os.path.exists(self.root_path + '/baseline/' + self.test_name):
+                shutil.rmtree(self.root_path + '/baseline/' + self.test_name)
+
+        else:
+            raise ValueError('Browser/App is not open')
+
+    def capture_full_screen(self):
+        test_name = self.test_name.replace(" ", "_")
+
+        if self.mode.lower() == "baseline":
+            path = self.root_path + '/baseline/' + test_name
+
+        elif self.mode.lower() == "test":
+            path = self.root_path + '/actual/' + test_name
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        print 'Capturing page...'
+        self.driver.save_screenshot(path + '/img' + str(self.count) + '.png')
+        self.count += 1
+
+    def capture_element(self, selector, value, left=0, top=0,right=0, bottom=0):
+        test_name = self.test_name.replace(" ", "_")
+
+        if self.mode.lower() == "baseline":
+            path = self.root_path + '/baseline/' + test_name
+
+        elif self.mode.lower() == "test":
+            path = self.root_path + '/actual/' + test_name
+
+        if selector.upper() == 'XPATH':
+            search_element = self.driver.find_element_by_xpath(value)
+        elif selector.upper() == 'ID':
+            search_element = self.driver.find_element_by_id(value)
+        elif selector.upper() == 'CLASS NAME':
+            search_element = self.driver.find_element_by_class_name(value)
+        elif selector.upper() == 'CSS SELECTOR':
+            search_element = self.driver.find_element_by_css_selector(value)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        print 'Capturing element...'
+        self.driver.save_screenshot(path + '/img' + str(self.count) + '.png')
+        location = search_element.location
+        size = search_element.size
+
+        self.driver.save_screenshot(path + '/img' + str(self.count) + '.png')
+        im = Image.open(path + '/img' + str(self.count) + '.png')
+
+        l = int(location['x'])
+        t = int(location['y'])
+        r = int(location['x'] + size['width'])
+        b = int(location['y'] + size['height'])
+        right = int(right)
+        bottom = int(bottom)
+        left = int(left)
+        top = int(top)
+        im = im.crop((left+l, top+t, right+r, bottom+b)) # defines crop points
+        im.save(path + '/img' + str(self.count) + '.png')
+        self.count += 1
+
+    def compare_images(self):
+        if self.env.lower() == 'local' and self.mode.lower() == 'test':
+            test_name = self.test_name.replace(" ", "_")
+            baseline_path = self.root_path + '/baseline/' + test_name
+            actual_path = self.root_path + '/actual/' + test_name
+            diff_path = self.root_path + '/diff/' + test_name
+
+            # compare actual and baseline images and save the diff image
+            for filename in os.listdir(actual_path):
+                if filename.endswith(".png"):
+                    a_path = ''
+                    b_path = ''
+                    d_path = ''
+
+                    if not os.path.exists(diff_path):
+                        os.makedirs(diff_path)
+
+                    b_path = baseline_path + "/" + filename
+                    a_path = actual_path + "/" + filename
+
+                    d_path = diff_path + '/' + filename
+
+                    proc = subprocess.Popen('compare -metric RMSE -subimage-search -dissimilarity-threshold 1.0 %s %s %s' % (a_path, b_path, d_path),
+                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                    out, err = proc.communicate()
+                    difference = err.split()[1][1:-1]
+                    output = open(actual_path + '/' + filename + '.txt', "a+")
+                    output.write(difference)
+                    output.close()
---
+++ report_generator.py
@@ -0,0 +1,107 @@
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+
+path = os.path.dirname(os.path.abspath(__file__))
+index = path.rfind('/')
+lib_folder = path[index + 1:]
+report_folder = path[:index]
+report_path = sys.argv[1:][0]
+
+if not os.path.exists(path + '/actual/'):
+    raise ValueError('Results directory does not exist')
+
+if not os.path.exists(path + '/baseline/'):
+    raise ValueError('Baseline directory does not exist')
+
+if not os.path.exists(path + '/diff/'):
+    raise ValueError('Diff directory does not exist')
+
+html = '''
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
+</head>
+<body>
+<h4 class="text-center">Visual test report</h4>
+<div class="container" style="position:relative;top:20px;">
+<table class="table table-striped table-hover" id="results">
+<thead>
+<tr>
+<th></th>
+<th>Test Name</th>
+</tr>
+</thead>
+<tbody>
+'''
+
+if '/' in report_path:
+    tree = ET.parse(report_path)
+    for t in tree.findall('.//test'):
+        test_name = t.get('name')
+        folder_name = test_name.replace(' ', '_')
+
+        html += '''<tr data-toggle="collapse" data-target='div[value="%s"]' class="accordion-toggle">
+        <td>
+        <button class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span></button>
+        </td>
+        <td>%s</td
+        </tr>
+        <tr>
+        <td colspan="12" class="hiddenRow"><div class="accordian-body collapse" value="%s">
+        <table class="table table-bordered table-hover">
+        <thead>
+        <tr>
+        <th>Baseline</th>
+        <th>Actual</th>
+        <th>Diff</th>
+        <th>Diff value</th>
+        </tr>
+        </thead>
+        <tbody>''' % (folder_name, test_name, folder_name)
+
+        for filename in os.listdir(path + '/actual/' + folder_name):
+            if filename.endswith(".png"):
+                actual_img_path = lib_folder + '/actual/' + folder_name + '/' + filename
+                baseline_img_path = lib_folder + '/baseline/' + folder_name + '/' + filename
+                diff_img_path = lib_folder + '/diff/' + folder_name + '/' + filename
+                html += '''
+                <tr>
+                <td><img src="%s" height="200" width="350"></td>
+                <td><img src="%s" height="200" width="350"></td>
+                <td><img src="%s" height="200" width="350"></td>
+                ''' % (baseline_img_path, actual_img_path, diff_img_path)
+
+            elif filename.endswith(".txt"):
+                infile = open(path + '/actual/' + folder_name + '/' + filename, 'r')
+                first_line = infile.readline().strip()
+                infile.close()
+                os.remove(path + '/actual/' + folder_name + '/' + filename)
+                html += '''
+                <td>%s</td></tr>''' % first_line
+
+        html += '''
+        </tbody>
+        </table>
+        </div>
+        </td>
+        </tr>'''
+
+    html += '''
+    </tbody>
+    </table>
+    </div>
+    </body>
+    </html>'''
+
+    print 'Creating report at %s/visualReport.html' % report_folder
+    output = open(report_folder + '/visualReport.html',"w")
+    output.write(html)
+    output.close()
