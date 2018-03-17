737ebfbad2fecc3e9281da9d357b489d63308249
Jesse Zacharias <jesse.zachariah@gmail.com>
2018-03-17 18:23:20 +0530

    Create constants.py

---
+++ RobotEyes/constants.py
@@ -0,0 +1,6 @@
+IMAGES_FOLDER = '/visual_images'
+ACTUAL_IMAGE_BASE_FOLDER = '/actual/'
+DIFF_IMAGE_BASE_FOLDER = '/diff/'
+BASELINE_IMAGE_BASE_FOLDER = '/baseline/'
+MODE_TEST = 'test'
+MODE_BASELINE = 'baseline'
