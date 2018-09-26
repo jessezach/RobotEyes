607b01762ff5fb8f8202fb5c4ca4eb0d68663950
Jesse <7547737+jz-jess@users.noreply.github.com>
2018-09-26 21:39:28 +0530

    updated bootstrap and jquery versions and using fontawesome for icon

--- RobotEyes/report_generator.py
+++ RobotEyes/report_generator.py
@@ -7,13 +7,12 @@ def generate_report(root_folder, report_path, img_path):
     html = '''
     <html>
     <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-                integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-                crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </head>
     <body>
     <h4 class="text-center">Visual test report</h4>
@@ -33,9 +32,9 @@ def generate_report(root_folder, report_path, img_path):
         test_name = t.get('name')
         folder_name = test_name.replace(' ', '_')
 
-        html += '''<tr data-toggle="collapse" data-target='div[value="%s"]' class="accordion-toggle">
+        html += '''<tr>
         <td>
-        <button class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span></button>
+        <button data-toggle="collapse" data-target='div[value="%s"]' class="btn btn-default btn-sm accordion-toggle"><i class="fas fa-arrow-right"></i></button>
         </td>
         <td>%s</td>
         </tr>
@@ -124,6 +123,17 @@ def generate_report(root_folder, report_path, img_path):
             else  {
              $('table#results > tbody > tr:nth-child(' + t + ') > td:nth-child(2)').css('color','green');
              t = t+2;
+            }
+          });
+          
+          $("button").click(function () {
+            c = $(this).find('i:first').attr("class");
+            c = c.split(" ");
+            cls = c[1];
+            if (cls === "fa-arrow-right") {
+                $(this).find('i:first').removeClass(cls).addClass("fa-arrow-down");
+            } else {
+                $(this).find('i:first').removeClass(cls).addClass("fa-arrow-right");
             }
           });
         });
