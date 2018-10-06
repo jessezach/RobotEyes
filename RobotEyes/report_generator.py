041454448b0b310724ce1d78329045e8fa79b253
Jesse Zacharias <iamjess988@gmail.com>
2018-10-06 16:42:36 +0530

    Release 0.57 (#10)
    * Optional argument "blur" in capture element and capture fullscreen keywords (#9)
    
    * option to blur regions in image
    
    * expose blur radius to tests
    
    * Updated bootstrap and jquery versions and using fontawesome for icons (#8)
    
    * updated bootstrap and jquery versions and using fontawesome for icon
    
    * optimising the jquery code
    
    * using google chart to show pass and fail count in a pie chart
    
    * use high chart instead of googlechart
    
    * make entire table row expand and collapsable
    
    * generate report within the results folder
    
    * check output dir before writing into file
    
    * version bump

--- RobotEyes/report_generator.py
+++ RobotEyes/report_generator.py
@@ -3,22 +3,24 @@ import sys
 import xml.etree.ElementTree as ET
 
 
-def generate_report(root_folder, report_path, img_path):
+def generate_report(root_folder, report_path, img_path, output_dir):
     html = '''
     <html>
     <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-                integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-                crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script src="https://code.highcharts.com/highcharts.js"></script>
+        <script src="http://code.highcharts.com/highcharts-3d.js"></script>
     </head>
     <body>
-    <h4 class="text-center">Visual test report</h4>
+    <div id="piechart"></div>
     <div class="container" style="position:relative;top:20px;">
-    <table class="table table-striped table-hover" id="results">
+    <table class="table table-striped table-sm" id="results">
     <thead>
     <tr>
     <th></th>
@@ -35,7 +37,7 @@ def generate_report(root_folder, report_path, img_path):
 
         html += '''<tr data-toggle="collapse" data-target='div[value="%s"]' class="accordion-toggle">
         <td>
-        <button class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span></button>
+        <button class="btn btn-default btn-sm"><i class="fas fa-arrow-right"></i></button>
         </td>
         <td>%s</td>
         </tr>
@@ -56,14 +58,14 @@ def generate_report(root_folder, report_path, img_path):
             if filename.endswith('.png'):
                 html += '''<tr>'''
                 if os.path.exists(img_path + '/baseline/' + folder_name + '/' + filename):
-                    actual_img_path = img_path + '/baseline/' + folder_name + '/' + filename
+                    actual_img_path = 'visual_images/baseline/' + folder_name + '/' + filename
                     html += '''<td><a href="%s" target="_blank"><img src="%s" height="200" width="350"></a></td>''' \
                             % (actual_img_path, actual_img_path)
                 else:
                     html += '''<td></td>'''
 
                 if os.path.exists(img_path + '/actual/' + folder_name + '/' + filename):
-                    baseline_img_path = img_path + '/actual/' + folder_name + '/' + filename
+                    baseline_img_path = 'visual_images/actual/' + folder_name + '/' + filename
                     html += '''<td><a href="%s" target="_blank"><img src="%s" height="200" width="350"></a></td>''' \
                             % (baseline_img_path, baseline_img_path)
 
@@ -72,12 +74,12 @@ def generate_report(root_folder, report_path, img_path):
 
                 arr = filename.split('.')
                 if os.path.exists(img_path + '/diff/' + folder_name + '/' + filename):
-                    diff_img_path = img_path + '/diff/' + folder_name + '/' + filename
+                    diff_img_path = 'visual_images/diff/' + folder_name + '/' + filename
                     html += '''<td><a href="%s" target="_blank"><img src="%s" height="200" width="350"></a></td>'''\
                             % (diff_img_path, diff_img_path)
                 
                 elif os.path.exists(img_path + '/diff/' + folder_name + '/' + arr[0] + '-0.png'):
-                    diff_img_path = img_path + '/diff/' + folder_name + '/' + arr[0] + '-0.png'
+                    diff_img_path = 'visual_images/diff/' + folder_name + '/' + arr[0] + '-0.png'
                     html += '''<td><a href="%s" target="_blank"><img src="%s" height="200" width="350"></a></td>'''\
                             % (diff_img_path, diff_img_path)
 
@@ -106,8 +108,15 @@ def generate_report(root_folder, report_path, img_path):
     </tbody>
     </table>
     </div>
+    <style>
+      tr.accordion-toggle:hover {
+        cursor:pointer;
+      }
+    </style>
     <script>
         $(document).ready(function() {
+          var pass = 0;
+          var fail = 0;
           var t = 1;
           $("table#innerResults").each(function() {
             var $items = $(this).find('tbody tr');
@@ -120,19 +129,78 @@ def generate_report(root_folder, report_path, img_path):
             if(color == 'rgb(255, 0, 0)') {
               $('table#results > tbody > tr:nth-child(' + t + ') > td:nth-child(2)').css('color','red');
               t = t+2;
+              fail = fail + 1;
             }
             else  {
              $('table#results > tbody > tr:nth-child(' + t + ') > td:nth-child(2)').css('color','green');
              t = t+2;
+             pass = pass + 1;
             }
+          });
+          
+          $("tr.accordion-toggle").click(function () {
+            classes = {
+                "fa-arrow-down": "fa-arrow-right",
+                "fa-arrow-right": "fa-arrow-down"
+            }
+            cls = $(this).find('i:first').attr("class").split(" ")[1];
+            $(this).find('i:first').removeClass(cls).addClass(classes[cls]);
+          });
+          
+          Highcharts.chart('piechart', {
+            chart: {
+                type: 'pie',
+                options3d: {
+                    enabled: true,
+                    alpha: 45,
+                    beta: 0
+                }
+            },
+            title: {
+                text: 'Test result stats'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.y:.1f}</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    depth: 35,
+                    colors: ['#32CD32', '#B22222'],
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.y:.1f}',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            series: [{
+                name: 'Count',
+                colorByPoint: true,
+                data: [{
+                    name: 'Pass',
+                    y: pass,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'Fail',
+                    y: fail
+                }]
+            }]
           });
         });
     </script>
     </body>
     </html>'''
 
-    print("Creating visual report at %s/visualReport.html" % root_folder)
-    output = open(root_folder + '/visualReport.html', 'w')
+    print("Creating visual report at %s/visualReport.html" % output_dir)
+    if output_dir == '':
+        output = open(root_folder + '/visualReport.html', 'w')
+    else:
+        output = open(root_folder + '/' + output_dir + '/visualReport.html', 'w')
     output.write(html)
     output.close()
 
@@ -157,6 +225,6 @@ if __name__ == "__main__":
         img_path = output_dir + '/visual_images'
 
     if os.path.exists(root_folder + "/" + report_path) and os.path.exists(root_folder + "/" + img_path):
-        generate_report(root_folder, report_path, img_path)
+        generate_report(root_folder, report_path, img_path, output_dir)
     else:
         raise Exception("Please provide a valid path to results.")
