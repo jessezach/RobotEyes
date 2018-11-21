2901f7ae8c80eeb97689e4a7027663f400e2238c
Jesse Zacharias <iamjess988@gmail.com>
2018-11-21 23:45:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ Visual regression library and report generator for robot framework. Capture elem
       author_email='iamjess988@gmail.com',
       url='https://github.com/jz-jess/RobotEyes',
       scripts=[os.path.join('scripts', 'reportgen')],
-      license='Apache License 2.0',
+      license='MIT',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=True,
