142db9ad0c91c453bbb208e9a0a8285c926e0856
Jesse <7547737+jz-jess@users.noreply.github.com>
2018-11-24 17:54:25 +0530

    enhancements to support parallel run of visual tests

--- RobotEyes/__init__.py
+++ RobotEyes/__init__.py
@@ -24,10 +24,11 @@ class RobotEyes(object):
         self.sys = platform.system()
         self.tolerance = tolerance
         self.stats = {}
+        self.content = ''
 
     def open_eyes(self, lib='Selenium2Library'):
-        self.output_dir = BuiltIn().replace_variables('${OUTPUT DIR}')
-        self.images_base_folder = self.output_dir + IMAGES_FOLDER
+        self.output_dir = self._output_dir()
+        self.images_base_folder = os.path.join(self.output_dir, IMAGES_FOLDER)
 
         try:
             s2l = BuiltIn().get_library_instance(lib)
@@ -53,16 +54,8 @@ class RobotEyes(object):
         # Delete visualReport.html if modified_at is older than a specified time difference.
         # Couldn't think of a better way to do this.
         # New report gets appended to old test run report if this is not done.
-        self._delete_report_if_old(self.output_dir + REPORT_FILE) if os.path.exists(self.output_dir + REPORT_FILE) else ''
-
-        if not os.path.exists(self.output_dir + REPORT_FILE):
-            file = open(self.output_dir + REPORT_FILE, 'w')
-            file.write(HEADER)
-            file.close()
-
-        with open(self.output_dir + REPORT_FILE, 'r') as file:
-            content = file.read()
-            self.content = content.replace(FOOTER, '')
+        self.report_path = os.path.join(self.output_dir, REPORT_FILE)
+        self._delete_report_if_old(self.report_path) if os.path.exists(self.report_path) else ''
 
     # Captures full screen
     def capture_full_screen(self, tolerance=None, blur=[], radius=50):
@@ -136,9 +129,9 @@ class RobotEyes(object):
     def compare_images(self):
         if self.mode.lower() == MODE_TEST:
             test_name = self.test_name.replace(' ', '_')
-            baseline_path = self.images_base_folder + BASELINE_IMAGE_BASE_FOLDER + test_name
-            actual_path = self.images_base_folder + ACTUAL_IMAGE_BASE_FOLDER + test_name
-            diff_path = self.images_base_folder + DIFF_IMAGE_BASE_FOLDER + test_name
+            baseline_path = os.path.join(self.images_base_folder, BASELINE_IMAGE_BASE_FOLDER, test_name)
+            actual_path = os.path.join(self.images_base_folder, ACTUAL_IMAGE_BASE_FOLDER, test_name)
+            diff_path = os.path.join(self.images_base_folder, DIFF_IMAGE_BASE_FOLDER, test_name)
 
             if not os.path.exists(diff_path):
                 os.makedirs(diff_path)
@@ -153,9 +146,9 @@ class RobotEyes(object):
                 d_path = ''
 
                 if filename.endswith('.png'):
-                    b_path = baseline_path + '/' + filename
-                    a_path = actual_path + '/' + filename
-                    d_path = diff_path + '/' + filename
+                    b_path = os.path.join(baseline_path, filename)
+                    a_path = os.path.join(actual_path, filename)
+                    d_path = os.path.join(diff_path, filename)
 
                     if os.path.exists(b_path):
                         self._resize(b_path, a_path)
@@ -189,7 +182,15 @@ class RobotEyes(object):
             self.content += INNER_TABLE_END
             self.content += FOOTER
 
-            file = open(self.output_dir + REPORT_FILE, 'w')
+            if os.path.exists(self.report_path):
+                with open(self.report_path, 'r') as file:
+                    old_content = file.read()
+                    old_content = old_content.replace(FOOTER, '')
+                    self.content = old_content + self.content
+            else:
+                self.content = HEADER + self.content
+
+            file = open(self.report_path, 'w')
             file.write(self.content)
             file.close()
 
@@ -252,26 +253,29 @@ class RobotEyes(object):
         return coord
 
     def _delete_folder_if_exists(self, test_name_folder):
-        if self.mode.lower() == MODE_TEST:
-            if os.path.exists(self.images_base_folder + ACTUAL_IMAGE_BASE_FOLDER + test_name_folder):
-                shutil.rmtree(self.images_base_folder + ACTUAL_IMAGE_BASE_FOLDER + test_name_folder)
+        actual_image_test_folder = os.path.join(self.images_base_folder, ACTUAL_IMAGE_BASE_FOLDER, test_name_folder)
+        diff_image_test_folder = os.path.join(self.images_base_folder, DIFF_IMAGE_BASE_FOLDER, test_name_folder)
+        baseline_image_test_folder = os.path.join(self.images_base_folder, BASELINE_IMAGE_BASE_FOLDER, test_name_folder)
 
-            if os.path.exists(self.images_base_folder + DIFF_IMAGE_BASE_FOLDER + test_name_folder):
-                shutil.rmtree(self.images_base_folder + DIFF_IMAGE_BASE_FOLDER + test_name_folder)
+        if self.mode.lower() == MODE_TEST:
+            if os.path.exists(actual_image_test_folder):
+                shutil.rmtree(actual_image_test_folder)
+
+            if os.path.exists(diff_image_test_folder):
+                shutil.rmtree(diff_image_test_folder)
 
         elif self.mode.lower() == MODE_BASELINE:
-            if os.path.exists(self.images_base_folder + BASELINE_IMAGE_BASE_FOLDER + test_name_folder):
-                shutil.rmtree(self.images_base_folder + BASELINE_IMAGE_BASE_FOLDER + test_name_folder)
-
+            if os.path.exists(baseline_image_test_folder):
+                shutil.rmtree(baseline_image_test_folder)
         else:
-            raise AssertionError('Mode should be test or baseline')
+            raise Exception('Mode should be test or baseline')
 
     def _create_empty_folder(self, test_name_folder):
         if self.mode.lower() == MODE_BASELINE:
-            self.path = self.images_base_folder + BASELINE_IMAGE_BASE_FOLDER + test_name_folder
+            self.path = os.path.join(self.images_base_folder, BASELINE_IMAGE_BASE_FOLDER, test_name_folder)
 
         elif self.mode.lower() == MODE_TEST:
-            self.path = self.images_base_folder + ACTUAL_IMAGE_BASE_FOLDER + test_name_folder
+            self.path = os.path.join(self.images_base_folder, ACTUAL_IMAGE_BASE_FOLDER, test_name_folder)
 
         if not os.path.exists(self.path):
             os.makedirs(self.path)
@@ -315,3 +319,11 @@ class RobotEyes(object):
         t2 = time.time()
         diff = int(t2 - t1)
         os.remove(path) if diff > REPORT_EXPIRATION_THRESHOLD else ''
+
+    def _output_dir(self):
+        output_dir = BuiltIn().replace_variables('${OUTPUT DIR}')
+
+        if 'pabot_results' in output_dir:
+            index = output_dir.find('/pabot_results')
+            return output_dir[:index]
+        return output_dir
