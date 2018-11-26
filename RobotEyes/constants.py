9b2274fca5a6316b9ba2d7a37dfe02c936ffee86
Jesse Zacharias <iamjess988@gmail.com>
2018-11-26 09:36:29 +0530

    Release 0.62 (#17)
    * Enhancements to support parallel run of visual tests (#16)

--- RobotEyes/constants.py
+++ RobotEyes/constants.py
@@ -1,8 +1,8 @@
-IMAGES_FOLDER = '/visual_images'
-ACTUAL_IMAGE_BASE_FOLDER = '/actual/'
-DIFF_IMAGE_BASE_FOLDER = '/diff/'
-BASELINE_IMAGE_BASE_FOLDER = '/baseline/'
+IMAGES_FOLDER = 'visual_images'
+ACTUAL_IMAGE_BASE_FOLDER = 'actual'
+DIFF_IMAGE_BASE_FOLDER = 'diff'
+BASELINE_IMAGE_BASE_FOLDER = 'baseline'
 MODE_TEST = 'test'
 MODE_BASELINE = 'baseline'
-REPORT_FILE = '/visualReport.html'
+REPORT_FILE = 'visualReport.html'
 REPORT_EXPIRATION_THRESHOLD = 5
