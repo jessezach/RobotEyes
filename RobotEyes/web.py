a3d420817fa536649f0a870eee30d7b30cb7a8ec
Jesse <7547737+jz-jess@users.noreply.github.com>
2019-01-13 23:47:29 +0530

    lot of changes

--- RobotEyes/web.py
+++ RobotEyes/web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request, jsonify
 from gevent import pywsgi
 import shutil
 import os
@@ -21,7 +21,7 @@ def overview():
             abs_directory = os.path.join(actual_path, directory)
 
             if os.path.isdir(abs_directory):
-                last_found = ''
+                last_found = ''  # incase we are inside an empty directory
 
                 for file in os.listdir(abs_directory):
                     if file.endswith('.txt'):
@@ -45,6 +45,98 @@ def overview():
 
     return render_template('overview.html', passed=passed, failed=failed, total=(passed+failed))
 
+
+@app.route("/passed")
+def passed():
+    data = {}
+
+    actual_path = os.path.join(results, 'visual_images', 'actual')
+
+    if os.path.exists(actual_path):
+        for directory in os.listdir(os.path.join(actual_path)):
+            test_name = directory.replace('_', ' ')
+
+            abs_directory = os.path.join(actual_path, directory)
+
+            if os.path.isdir(abs_directory):
+                for file in os.listdir(abs_directory):
+                    if file.endswith('.txt'):
+                        f = os.path.join(abs_directory, file)
+
+                        obj = open(f, 'r')
+                        first_line = obj.readline().strip()
+                        arr = first_line.split()
+                        obj.close()
+                        color = arr[1]
+
+                        if color == 'red':
+                            break
+                else:
+                    data[test_name] = directory
+
+    return render_template('passed.html', passed=data)
+
+
+@app.route("/failed")
+def failed():
+    data = []
+
+    actual_path = os.path.join(results, 'visual_images', 'actual')
+
+    if os.path.exists(actual_path):
+        for directory in os.listdir(os.path.join(actual_path)):
+            test_name = directory.replace('_', ' ')
+
+            abs_directory = os.path.join(actual_path, directory)
+
+            if os.path.isdir(abs_directory):
+
+                for file in os.listdir(abs_directory):
+
+                    if file.endswith('.txt'):
+                        f = os.path.join(abs_directory, file)
+
+                        obj = open(f, 'r')
+                        first_line = obj.readline().strip()
+                        arr = first_line.split()
+                        obj.close()
+                        color = arr[1]
+
+                        if color == 'red':
+                            data.append([test_name, directory])
+                            break
+
+    return render_template('failed.html', failed=data)
+
+@app.route('/<test>')
+def images(test):
+    test_name = test.replace('_', ' ')
+    output = {test_name: []}
+
+    baseline_directory = os.path.join(results, 'visual_images', 'baseline', test)
+    actual_directory = os.path.join(results, 'visual_images', 'actual', test)
+    diff_directory = os.path.join(results, 'visual_images', 'diff', test)
+
+    for file in os.listdir(actual_directory):
+        if file.endswith('.png'):
+            images = []
+            images.append(os.path.join(baseline_directory, file))
+            images.append(os.path.join(actual_directory, file))
+            images.append(os.path.join(diff_directory, file))
+
+            file = file + '.txt'
+            f = os.path.join(actual_directory, file)
+
+            obj = open(f, 'r')
+            first_line = obj.readline().strip()
+            arr = first_line.split()
+            obj.close()
+            color = arr[1]
+            value = arr[0]
+            images.append(color)
+            images.append(value)
+            output[test_name].append(images)
+    return render_template('test.html', data=output)
 
 @app.route("/all")
 def report():
@@ -105,6 +197,23 @@ def make_all_baseline():
 
     flash('Successfully moved all images.')
     return redirect(url_for('report'))
+
+
+@app.route("/baseline_images", methods=['POST'])
+def baseline_images():
+    imges = request.json['images']
+
+    for img in imges:
+        actual_path = os.path.join(results, img)
+        baseline_path = actual_path.replace('/actual/', '/baseline/')
+
+        if os.path.exists(actual_path):
+            if os.path.exists(baseline_path):
+                os.remove(baseline_path)
+            shutil.copyfile(actual_path, baseline_path)
+
+    resp = {'success': True}
+    return jsonify(resp)
 
 
 @app.after_request
