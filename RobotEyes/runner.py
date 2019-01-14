8216d4d933808f35948dcdab3b926d1eb5e6c3fa
Jesse Zacharias <iamjess988@gmail.com>
2019-01-14 17:12:37 +0530

    Flask based interactive report (#21)
    * flask based report

---
+++ RobotEyes/runner.py
@@ -0,0 +1,37 @@
+import sys
+from optparse import OptionParser
+
+from . import web
+
+
+def parse_options():
+    parser = OptionParser()
+
+    parser.add_option(
+        '--results',
+        dest='results',
+        default='',
+        help="relative path to results folder where visual_images directory exists"
+    )
+
+    parser.add_option(
+        '--host',
+        dest='host',
+        default='0.0.0.0',
+        help="host on which server should run"
+    )
+
+    parser.add_option(
+        '--port',
+        dest='port',
+        default=5000,
+        help="port on which server should run"
+    )
+    opts, args = parser.parse_args()
+    return parser, opts, args
+
+
+def main():
+    parser, opts, arguments = parse_options()
+
+    web.start(opts.results, opts.host, opts.port)
