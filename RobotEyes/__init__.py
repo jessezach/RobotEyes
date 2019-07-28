ae61fd83461129710fb055d92a6b5fbc7e7cdaaa
Jesse <7547737+jz-jess@users.noreply.github.com>
2019-07-28 23:36:39 +0530

    major refactor

--- RobotEyes/__init__.py
+++ RobotEyes/__init__.py
@@ -1,46 +1,35 @@
-import os
+from datetime import datetime
 import shutil
 import time
-import subprocess
-import platform
-import math
-from datetime import datetime
 
-from PIL import Image, ImageFilter
+from PIL import Image
 from robot.libraries.BuiltIn import BuiltIn
 
 from .constants import *
 from .report_utils import *
+from .imagemagick import Imagemagick
+from .selenium_hooks import SeleniumHooks
 
 
 class RobotEyes(object):
     output_dir = ''
     images_base_folder = ''
-    driver = None
     test_name = ''
-    count = 0
+    baseline_dir = ''
+    browser = None
 
     def __init__(self, tolerance=0):
-        self.sys = platform.system()
         self.tolerance = float(tolerance)
         self.tolerance = self.tolerance/100 if self.tolerance >= 1 else self.tolerance
         self.stats = {}
-        # self.content = ''
         self.fail = False
 
     def open_eyes(self, lib='SeleniumLibrary'):
         self.baseline_dir = self._get_baseline_dir()
         self.output_dir = self._output_dir()
         self.images_base_folder = os.path.join(self.output_dir, IMAGES_FOLDER)
-
-        try:
-            s2l = BuiltIn().get_library_instance(lib)
-            self.driver = s2l._current_application() if lib == 'AppiumLibrary' else s2l._current_browser()
-        except RuntimeError:
-            raise Exception('%s instance not found' % lib)
-
+        self.browser = SeleniumHooks(lib)
         self.test_name = BuiltIn().replace_variables('${TEST NAME}')
-        self.count = 1
         test_name_folder = self.test_name.replace(' ', '_')
 
         # delete if directory already exist. Fresh test
@@ -53,56 +42,31 @@ class RobotEyes(object):
     def capture_full_screen(self, tolerance=None, blur=[], radius=50):
         tolerance = float(tolerance) if tolerance else self.tolerance
         tolerance = tolerance/100 if tolerance >= 1 else tolerance
-        self.driver.save_screenshot(self.path + '/img' + str(self.count) + '.png')
-        self._blur_regions(blur, radius) if blur else ''
-        self._resize(self.path + '/img' + str(self.count) + '.png')
-        key = 'img' + str(self.count) + '.png'
+        count = self.browser.capture_full_screen(self.path)
+        self.browser.blur_regions(blur, radius, self.path) if blur else ''
+        self._resize(self.path + '/img' + str(count) + '.png')
+        key = 'img' + str(count) + '.png'
         self.stats[key] = tolerance
-        self.count += 1
+
 
     # Captures a specific region in a mobile screen
     def capture_mobile_element(self, selector, tolerance=None):
         tolerance = float(tolerance) if tolerance else self.tolerance
-        prefix, locator, search_element = self._find_element(selector)
-        location = search_element.location
-        size = search_element.size
-        self.driver.save_screenshot(self.path + '/img' + str(self.count) + '.png')
-        left = location['x']
-        top = location['y']
-        right = location['x'] + size['width']
-        bottom = location['y'] + size['height']
-        image = Image.open(self.path + '/img' + str(self.count) + '.png')
-        image = image.crop((left, top, right, bottom))
-        image.save(self.path + '/img' + str(self.count) + '.png')
-        key = 'img' + str(self.count) + '.png'
+        count = self.browser.capture_mobile_element(selector, self.path)
+        key = 'img' + str(count) + '.png'
         self.stats[key] = tolerance
-        self.count += 1
 
     # Captures a specific region in a webpage
     def capture_element(self, selector, tolerance=None, blur=[], radius=50):
         tolerance = float(tolerance) if tolerance else self.tolerance
         tolerance = tolerance/100 if tolerance >= 1 else tolerance
-        prefix, locator, _ = self._find_element(selector)
         time.sleep(1)
-        self.driver.save_screenshot(self.path + '/img' + str(self.count) + '.png')
-        coord = self._get_coordinates(prefix, locator)
-        left = math.ceil(coord['left'])
-        top = math.ceil(coord['top'])
-        right = math.ceil(coord['right'])
-        bottom = math.ceil(coord['bottom'])
-        self._blur_regions(blur, radius) if blur else ''
-        left, right, top, bottom = self._update_coordinates(left, right, top, bottom)
-        im = Image.open(self.path + '/img' + str(self.count) + '.png')
-        im = im.crop((left, top, right, bottom))
-        im.resize((1024, 700), Image.ANTIALIAS)
-        im.save(self.path + '/img' + str(self.count) + '.png')
-        key = 'img' + str(self.count) + '.png'
+        count = self.browser.capture_element(self.path, selector, blur, radius)
+        key = 'img' + str(count) + '.png'
         self.stats[key] = tolerance
-        self.count += 1
 
     def scroll_to_element(self, selector):
-        prefix, locator, search_element = self._find_element(selector)
-        self.driver.execute_script("return arguments[0].scrollIntoView();", search_element)
+        self.browser.scroll_to_element(selector)
 
     def compare_images(self):
         test_name = self.test_name.replace(' ', '_')
@@ -112,7 +76,6 @@ class RobotEyes(object):
 
         if not os.path.exists(diff_path):
             os.makedirs(diff_path)
-        # self.content += make_parent_row(self.test_name)
 
         # compare actual and baseline images and save the diff image
         for filename in os.listdir(actual_path):
@@ -126,7 +89,7 @@ class RobotEyes(object):
                 d_path = os.path.join(diff_path, filename)
 
                 if os.path.exists(b_path):
-                    difference = self._compare(b_path, a_path, d_path)
+                    difference = Imagemagick(b_path, a_path).compare_images(d_path)
 
                     try:
                         threshold = float(self.stats[filename])
@@ -135,88 +98,16 @@ class RobotEyes(object):
 
                     color, result = self._get_result(difference, threshold)
                     text = '%s %s' % (result, color)
-                    # self.content += make_image_row(b_path, a_path, d_path, [color, result])
                 else:
                     shutil.copy(a_path, b_path)
                     text = '%s %s' % ('None', 'green')
-                    # self.content += make_image_row(b_path, a_path, d_path, ['green', 'None'])
 
                 output = open(actual_path + '/' + filename + '.txt', 'w')
                 output.write(text)
                 output.close()
-
-        # self.content += INNER_TABLE_END
-        # self.content += FOOTER
-        # self._write_to_report()
         BuiltIn().run_keyword('Fail', 'Image dissimilarity exceeds tolerance') if self.fail else ''
 
-    def _compare(self, b_path, a_path, d_path):
-        compare_cmd = 'compare -metric RMSE -subimage-search -dissimilarity-threshold 1.0 "%s" "%s" "%s"' \
-                      % (a_path, b_path, d_path)
 
-        attempts = 0
-        while attempts < 2:
-            proc = subprocess.Popen(compare_cmd,
-                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-
-            out, err = proc.communicate()
-            print('Comparison output: %s' % err)
-            diff = err.split()[1][1:-1]
-            trimmed = diff[0:4] if len(diff) >= 4 else diff
-
-            try:
-                trimmed = float(trimmed)
-                return trimmed
-            except ValueError:
-                if attempts == 0:
-                    print('Comparison failed. Trying again..')
-                    compare_cmd = 'magick ' + compare_cmd
-                else:
-                    raise Exception('Could parse comparison output: %s' % err)
-            finally:
-                attempts += 1
-
-    def _find_element(self, selector):
-        if selector.startswith('//'):
-            prefix = 'xpath'
-            locator = selector
-        else:
-            selector_parts = selector.partition('=')
-            prefix = selector_parts[0].strip()
-            locator = selector_parts[2].strip()
-            if not locator:
-                raise Exception('Please prefix locator type.')
-        dict = {
-            'xpath': self.driver.find_element_by_xpath,
-            'id': self.driver.find_element_by_id,
-            'class': self.driver.find_element_by_class_name,
-            'css': self.driver.find_element_by_css_selector
-        }
-        if prefix.lower() not in dict:
-            raise Exception('Please add a valid locator prefix. Eg xpath, css, class.')
-
-        func = dict[prefix.lower()]
-        search_element = func(locator)
-        return prefix, locator, search_element
-
-    def _get_coordinates(self, prefix, locator):
-        if prefix.lower() == 'xpath':
-            locator = locator.replace('"', "'")
-            cmd = "var e = document.evaluate(\"{0}\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)" \
-                  ".singleNodeValue;return e.getBoundingClientRect();".format(locator)
-            coord = self.driver.execute_script(cmd)
-        elif prefix.lower() == 'css':
-            locator = locator.replace('"', "'")
-            cmd = "var e = document.querySelector(\"{0}\");return e.getBoundingClientRect();".format(locator)
-            coord = self.driver.execute_script(cmd)
-        elif prefix.lower() == 'id':
-            cmd = "var e = document.getElementById(\"{0}\");return e.getBoundingClientRect();".format(locator)
-            coord = self.driver.execute_script(cmd)
-        elif prefix.lower() == 'class':
-            cmd = "var e = document.getElementsByClassName(\"{0}\")[0];return e.getBoundingClientRect();" \
-                .format(locator)
-            coord = self.driver.execute_script(cmd)
-        return coord
 
     def _delete_folder_if_exists(self, test_name_folder):
         actual_image_test_folder = os.path.join(self.images_base_folder, ACTUAL_IMAGE_BASE_FOLDER, test_name_folder)
@@ -238,21 +129,6 @@ class RobotEyes(object):
 
         if not os.path.exists(self.path):
             os.makedirs(self.path)
-
-    def _blur_regions(self, selectors, radius):
-        selectors = selectors if isinstance(selectors, list) else [selectors]
-
-        for region in selectors:
-            prefix, locator, _ = self._find_element(region)
-            area_coordinates = self._get_coordinates(prefix, locator)
-            left, right = math.ceil(area_coordinates['left']), math.ceil(area_coordinates['right'])
-            top, bottom = math.ceil(area_coordinates['top']), math.ceil(area_coordinates['bottom'])
-            left, right, top, bottom = self._update_coordinates(left, right, top, bottom)
-            im = Image.open(self.path + '/img' + str(self.count) + '.png')
-            cropped_image = im.crop((left, top, right, bottom))
-            blurred_image = cropped_image.filter(ImageFilter.GaussianBlur(radius=float(radius)))
-            im.paste(blurred_image, (left, top, right, bottom))
-            im.save(self.path + '/img' + str(self.count) + '.png')
 
     def _resize(self, *args):
         for arg in args:
@@ -288,19 +164,6 @@ class RobotEyes(object):
             result = '%s>%s' % (threshold, difference)
         return color, result
 
-    def _write_to_report(self):
-        if os.path.exists(self.report_path):
-            with open(self.report_path, 'r') as file:
-                old_content = file.read()
-                old_content = old_content.replace(FOOTER, '')
-                self.content = old_content + self.content
-        else:
-            self.content = HEADER + self.content
-
-        file = open(self.report_path, 'w')
-        file.write(self.content)
-        file.close()
-
     def _get_baseline_dir(self):
         baseline_dir = BuiltIn().replace_variables('${images_dir}')
         if baseline_dir is None:
@@ -308,11 +171,3 @@ class RobotEyes(object):
         baseline_dir = os.path.join(os.getcwd(), baseline_dir)
         os.makedirs(baseline_dir) if not os.path.exists(baseline_dir) else ''
         return baseline_dir
-
-    def _update_coordinates(self, left, right, top, bottom):
-        if self.sys.lower() == "darwin":
-            left = left * 2
-            right = right * 2
-            top = top * 2
-            bottom = bottom * 2
-        return left, right, top, bottom
